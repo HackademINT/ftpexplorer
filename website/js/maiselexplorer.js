407505d4b0617ff2c2e1174423f52658b9995a67
Nicolas BONNET <nicolas.bonnet@telecom-sudparis.eu>
2016-11-09 12:36:30 +0100

    Ajout de commentaires dans le javascript.

--- website/js/maiselexplorer.js
+++ website/js/maiselexplorer.js
@@ -1,16 +1,32 @@
-var serverPort = 9999;
+/*
+ * Ce script sera execute sur les clients apres qu'ils aient teleche la page
+ * sur me.minet.net. Il a pour role de completer la page en allant chercher
+ * les informations sur les fichiers presents sur les serveurs FTP 
+ */
 
-var ftp_servers_sockets = [];
 
+var serverPort = 9999; // Port sur lequel tourne le script python de ftp_server
+var ftp_servers_sockets = []; // On stocke les sockets ouvertes
+
+// TODO: supprimer cette ligne
 ftp_servers.push("127.0.0.1"); // On ajoute le serveur en local pour les tests.
 
 
 function clearFileList() {
+    /*
+     * Vide la liste des fichiers
+     */
     var champ = document.getElementById("me_fichiers")
     champ.innerHTML = ""
 }
 
 function addFileToList(file, server) {
+    /*
+     * Ajoute une ligne a la liste des fichiers
+     * Prend en argument un tableau sous le format : [nom, taille, date]
+     * Prend en argument l'ip du serveur de provenance
+     */
+
     var champ = document.getElementById("me_fichiers")
     
     var code = ""
@@ -39,6 +55,7 @@ for(i=0; i<ftp_servers.length; i++) {
     var curServ = ftp_servers[i];
     var socket = io.connect('http://' + curServ + ':' + serverPort  + '/me');
     ftp_servers_sockets.push(socket);
+    
     socket.on("connect", function() { 
 
         // Quand on est connecte au serveur, on envoie une requete pour les
@@ -48,6 +65,7 @@ for(i=0; i<ftp_servers.length; i++) {
             
             console.log("Got a response !");
             
+            // On affiche tous les fichiers du FTP
             var files = JSON.parse(data);
             clearFileList();
             for( var i in files ) {
@@ -59,19 +77,30 @@ for(i=0; i<ftp_servers.length; i++) {
     });
 }
 
+
 document.getElementById("me_recherche").onsubmit = function() {
+    /*
+     * Execute lorsque l'utilisateur fait une recherche
+     */
+
+    // On envoie une requete a chaque serveur
     for(i=0; i<ftp_servers.length; i++) {
+
         var socket = ftp_servers_sockets[i]; 
+        // On envoie au serveur le mot recherche
         socket.emit("me_search", document.getElementById("me_recherche_champ").value, function( data ) {
                 
             console.log("Got a response !");
+
+            // Et on complete la liste...
             var files = JSON.parse(data);
             clearFileList();
             for( var i in files ) {
                 addFileToList( files[i], curServ ); 
             }
+
         });
     }
-    return false;
+    return false; // Empeche la page de se rafraichir apres la recherche
 };
 
